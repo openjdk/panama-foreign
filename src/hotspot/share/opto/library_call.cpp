94f27131b385922f1f0bd5ff97ff91785b8c5363
Razvan Lupusoru <rlupusoru@openjdk.org>
2018-05-24 11:27:27 -0700

    Fix inline_vector_extract not registering node with gvn

--- src/hotspot/share/opto/library_call.cpp
+++ src/hotspot/share/opto/library_call.cpp
@@ -8000,7 +8000,7 @@ bool LibraryCallKit::inline_vector_extract() {
     return false;
   }
 
-  Node* operation = ExtractNode::make(opd, idx->get_con(), elem_bt);
+  Node* operation = gvn().transform(ExtractNode::make(opd, idx->get_con(), elem_bt));
 
   Node* bits = NULL;
   switch (elem_bt) {
