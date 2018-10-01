ca648aba85463a36dc7a7a7ae83db54631c91fd6
Jorn Vernee <jbvernee@xs4all.nl>
2018-10-01 13:24:05 +0100

    8211275: Add test
    Reviewed-by: sundar

---
+++ test/jdk/java/foreign/SignatureMismatchTest.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright (c) 2018, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+
+/*
+ * @test
+ * @run testng SignatureMismatchTest
+ */
+
+import java.foreign.Libraries;
+import java.foreign.Scope;
+import java.foreign.annotations.NativeCallback;
+import java.foreign.annotations.NativeHeader;
+import java.foreign.memory.Pointer;
+
+import static java.lang.invoke.MethodHandles.lookup;
+
+import org.testng.annotations.*;
+
+import static org.testng.Assert.*;
+
+@Test
+public class SignatureMismatchTest {
+
+    @NativeHeader(declarations = 
+        "exit=()v" // missing param
+    )
+    interface Helper1 {
+        void exit(int code);
+    }
+
+    @NativeHeader(declarations = 
+        "exit=(i32)i32" // return mismatch
+    )
+    interface Helper2 {
+        void exit(int code);
+    }
+
+    @NativeHeader(declarations = 
+        "printf=(u64:u8*)i32"
+    )
+    interface Helper3 {
+        int printf(Pointer<Byte> message); // missing parameter for varargs
+    }
+
+    @NativeCallback("(i32)v")
+    interface Func {
+        int m(int i); //return mismatch
+    }
+
+
+    @Test(expectedExceptions = RuntimeException.class)
+    void testAritiyMismatch() {
+        Libraries.bind(lookup(), Helper1.class);
+    }
+
+    @Test(expectedExceptions = RuntimeException.class)
+    void testReturnMismatch() {
+        Libraries.bind(lookup(), Helper2.class);
+    }
+
+    @Test(expectedExceptions = RuntimeException.class)
+    void testVaridadicMismatch() {
+        Libraries.bind(lookup(), Helper3.class);
+    }
+
+    @Test(expectedExceptions = RuntimeException.class)
+    void testCallbackMismatch() {
+        try (Scope sc = Scope.newNativeScope()) {
+            sc.allocateCallback(Func.class, i -> i);
+        }
+    }
+}
