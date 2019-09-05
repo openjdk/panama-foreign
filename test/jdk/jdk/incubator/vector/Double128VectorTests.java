ac55a53a20139fb18372bc502f7618843667df38
Yang Zhang <yzhang@openjdk.org>
2019-09-05 16:16:54 +0800

    Update jtreg test cases to make sure that C2 is used

--- test/jdk/jdk/incubator/vector/Double128VectorTests.java
+++ test/jdk/jdk/incubator/vector/Double128VectorTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018, 2019 Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -24,7 +24,7 @@
 /*
  * @test
  * @modules jdk.incubator.vector
- * @run testng/othervm -ea -esa Double128VectorTests
+ * @run testng/othervm -ea -esa -Xbatch Double128VectorTests
  */
 
 // -- This file was mechanically generated: Do not edit! -- //
--- test/jdk/jdk/incubator/vector/Long512VectorTests.java
+++ test/jdk/jdk/incubator/vector/Long512VectorTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2018, 2019 Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -24,7 +24,7 @@
 /*
  * @test
  * @modules jdk.incubator.vector
- * @run testng/othervm -ea -esa Long512VectorTests
+ * @run testng/othervm -ea -esa -Xbatch Long512VectorTests
  */
 
 // -- This file was mechanically generated: Do not edit! -- //
