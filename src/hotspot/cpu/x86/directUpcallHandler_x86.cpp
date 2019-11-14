e87d44b321a10397586fef6362988ea1cc5b44dd
Athijegannathan Sundararajan <sundar@openjdk.org>
2019-11-14 18:18:05 +0530

    8234172: Panama foreign branch build failure with src/hotspot/cpu/x86/directUpcallHandler_x86.cpp
    Reviewed-by: mcimadamore, jvernee

--- src/hotspot/cpu/x86/directUpcallHandler_x86.cpp
+++ src/hotspot/cpu/x86/directUpcallHandler_x86.cpp
@@ -152,7 +152,8 @@ struct upcall_context {
   assert(thread->is_Java_thread(), "must be"); \
   ThreadInVMfromNative __tiv((JavaThread *)thread); \
   JavaValue result(RES_T); \
-  JavaCalls::call(&result, meth, &args, thread);
+  methodHandle mhandle(thread, meth); \
+  JavaCalls::call(&result, mhandle, &args, thread);
 
 
 static long specialized_upcall_helper_J(long l0, long l1, long l2, long l3,
