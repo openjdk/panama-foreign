6ecd9d982dfe2e810fcbe8464a529ad5eae7ee48
Nick Gasson <ngasson@openjdk.org>
2019-11-19 16:43:02 +0800

    8234238: AArch64: foreign build broken in directUpcallHandler_aarch64.cpp
    See 8234172
    
    Reviewed-by: mcimadamore

--- src/hotspot/cpu/aarch64/directUpcallHandler_aarch64.cpp
+++ src/hotspot/cpu/aarch64/directUpcallHandler_aarch64.cpp
@@ -145,7 +145,8 @@ struct upcall_context {
                                                                         \
   ThreadInVMfromNative __tiv((JavaThread *)thread);                     \
   JavaValue result(RES_T);                                              \
-  JavaCalls::call(&result, meth, &args, thread);
+  methodHandle mhandle(thread, meth);                                   \
+  JavaCalls::call(&result, mhandle, &args, thread);
 
 
 static long specialized_upcall_helper_J(long l0, long l1, long l2, long l3,
