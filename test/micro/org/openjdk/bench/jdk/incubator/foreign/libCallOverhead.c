9ef78a5e3cc01f410a18633be9f98ae56ff756cc
Jorn Vernee <jvernee@openjdk.org>
2020-03-26 10:17:55 +0000

    8237573: Add a basic call overhead benchmark
    Reviewed-by: mcimadamore

--- test/micro/org/openjdk/bench/jdk/incubator/foreign/libCallOverhead.c
+++ test/micro/org/openjdk/bench/jdk/incubator/foreign/libCallOverhead.c
@@ -20,44 +20,15 @@
  * or visit www.oracle.com if you need additional information or have any
  * questions.
  */
-package org.openjdk.bench.jdk.incubator.foreign.points.support;
 
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
+#ifdef _WIN64
+#define EXPORT __declspec(dllexport)
+#else
+#define EXPORT
+#endif
 
-public class BBPoint {
+EXPORT void func() {}
 
-    static {
-        System.loadLibrary("JNIPoint");
-    }
-
-    private final ByteBuffer buff;
-
-    public BBPoint(int x, int y) {
-        this.buff = ByteBuffer.allocateDirect(4 * 2).order(ByteOrder.nativeOrder());
-        setX(x);
-        setY(y);
-    }
-
-    public void setX(int x) {
-        buff.putInt(0, x);
-    }
-
-    public int getX() {
-        return buff.getInt(0);
-    }
-
-    public int getY() {
-        return buff.getInt(1);
-    }
-
-    public void setY(int y) {
-        buff.putInt(0, y);
-    }
-
-    public double distanceTo(BBPoint other) {
-        return distance(buff, other.buff);
-    }
-
-    private static native double distance(ByteBuffer p1, ByteBuffer p2);
+EXPORT int identity(int x) {
+  return x;
 }
