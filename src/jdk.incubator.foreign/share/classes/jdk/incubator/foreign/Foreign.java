3e3513a85801431e601c7f174fa7fd27939789c2
Maurizio Cimadamore <maurizio.cimadamore@oracle.com>
2020-03-30 19:04:47 +0100

    Added standard disclaimer for restricted methods

--- src/jdk.incubator.foreign/share/classes/jdk/incubator/foreign/Foreign.java
+++ src/jdk.incubator.foreign/share/classes/jdk/incubator/foreign/Foreign.java
@@ -104,6 +104,10 @@ public interface Foreign {
      * Since the returned segment can be effectively accessed by multiple threads in an unconstrained fashion,
      * this method should be used with care, as it might lead to JVM crashes - e.g. in the case where a thread {@code A}
      * closes a segment while another thread {@code B} is accessing it.
+     * <p>
+     * This method is <em>restricted</em>. Restricted method are unsafe, and, if used incorrectly, their use might crash
+     * the JVM crash or, worse, silently result in memory corruption. Thus, clients should refrain from depending on
+     * restricted methods, and use safe and supported functionalities, where possible.
      * @param segment the segment from which an unconfined view is to be created.
      * @return a non-confined memory segment that has the same spatial and temporal bounds as the provided segment.
      */
