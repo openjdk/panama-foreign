e1ae7db0e2117bca21a154981c125dabaceebe92
Peter Levart <plevart@openjdk.org>
2020-05-08 14:56:32 +0000

    Alternative scalable MemoryScope
    Reviewed-by: mcimadamore, psandoz

--- src/jdk.incubator.foreign/share/classes/jdk/internal/foreign/HeapMemorySegmentImpl.java
+++ src/jdk.incubator.foreign/share/classes/jdk/internal/foreign/HeapMemorySegmentImpl.java
@@ -121,7 +121,7 @@ public class HeapMemorySegmentImpl<H> extends AbstractMemorySegmentImpl {
 
     static <Z> HeapMemorySegmentImpl<Z> makeHeapSegment(Supplier<Z> obj, int length, int base, int scale) {
         int byteSize = length * scale;
-        MemoryScope scope = new MemoryScope(null, null);
+        MemoryScope scope = MemoryScope.create(null, null);
         return new HeapMemorySegmentImpl<>(base, obj, byteSize, defaultAccessModes(byteSize), Thread.currentThread(), scope);
     }
 }
