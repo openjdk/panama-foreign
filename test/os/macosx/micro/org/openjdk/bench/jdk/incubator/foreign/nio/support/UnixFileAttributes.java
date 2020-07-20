3f7ff666ab4ae41547a631343a6bcec62a8d5afe
Henry Jen <henry.jen@oracle.com>
2020-07-20 08:13:45 -0700

    Dup UnixFileAttribute for platform specific code for now

--- test/os/macosx/micro/org/openjdk/bench/jdk/incubator/foreign/nio/support/UnixFileAttributes.java
+++ test/os/macosx/micro/org/openjdk/bench/jdk/incubator/foreign/nio/support/UnixFileAttributes.java
@@ -55,6 +55,58 @@ public class UnixFileAttributes {
         super();
     }
 
+    UnixFileAttributes(LibC.stat64 buf) {
+        st_mode = buf.st_mode$get();
+        st_ino = buf.st_ino$get();
+        st_dev = buf.st_dev$get();
+        st_rdev = buf.st_rdev$get();
+        st_nlink = buf.st_nlink$get();
+        st_uid = buf.st_uid$get();
+        st_gid = buf.st_gid$get();
+        st_size = buf.st_size$get();
+        st_atime_sec = buf.st_atimespec$get().tv_sec$get();
+        st_mtime_sec = buf.st_mtimespec$get().tv_sec$get();
+        st_ctime_sec = buf.st_ctimespec$get().tv_sec$get();
+        st_atime_nsec = buf.st_atimespec$get().tv_nsec$get();
+        st_mtime_nsec = buf.st_mtimespec$get().tv_nsec$get();
+        st_ctime_nsec = buf.st_ctimespec$get().tv_nsec$get();
+        st_birthtime_sec = buf.st_birthtimespec$get().tv_sec$get();
+    }
+
+    UnixFileAttributes(MemoryAddress buf) {
+        try {
+            st_mode = (int) LibC.stat64.st_mode$VH.get(buf);
+            st_ino = (long) LibC.stat64.st_ino$VH.get(buf);
+            st_dev = (long) LibC.stat64.st_dev$VH.get(buf);
+            st_rdev = (long) LibC.stat64.st_rdev$VH.get(buf);
+            st_nlink = (int) LibC.stat64.st_nlink$VH.get(buf);
+            st_uid = (int) LibC.stat64.st_uid$VH.get(buf);
+            st_gid = (int) LibC.stat64.st_gid$VH.get(buf);
+            st_size = (long) LibC.stat64.st_size$VH.get(buf);
+            MemoryAddress ts = buf.addOffset(LibC.stat64.st_atimespec$OFFSET);
+            st_atime_sec = (long) LibC.timespec.tv_sec$VH.get(ts);
+            st_atime_nsec = (long) LibC.timespec.tv_nsec$VH.get(ts);
+
+            ts = buf.addOffset(LibC.stat64.st_mtimespec$OFFSET);
+            st_mtime_sec = (long) LibC.timespec.tv_sec$VH.get(ts);
+            st_mtime_nsec = (long) LibC.timespec.tv_nsec$VH.get(ts);
+
+            ts = buf.addOffset(LibC.stat64.st_ctimespec$OFFSET);
+            st_ctime_sec = (long) LibC.timespec.tv_sec$VH.get(ts);
+            st_ctime_nsec = (long) LibC.timespec.tv_nsec$VH.get(ts);
+
+            ts = buf.addOffset(LibC.stat64.st_birthtimespec$OFFSET);
+            st_birthtime_sec = (long) LibC.timespec.tv_sec$VH.get(ts);
+        } catch (Throwable ex) {
+            throw new AssertionError(ex);
+        }
+    }
+
+    public static UnixFileAttributes from(MemoryAddress buf) {
+        return new UnixFileAttributes(buf);
+    }
+
+    // package-private
     public int mode()  { return st_mode; }
     public long ino()  { return st_ino; }
     public long dev()  { return st_dev; }
