e55d86fb8ff82434492adc6deac4a9f80df8939e
Nick Gasson <nick.gasson@arm.com>
2020-11-11 14:05:58 +0800

    Fix C2 assert failure and disable TestIntrinsics on AArch64 for now

--- test/jdk/java/foreign/TestIntrinsics.java
+++ test/jdk/java/foreign/TestIntrinsics.java
@@ -23,6 +23,7 @@
 
 /*
  * @test
+ * @requires os.arch=="amd64"
  * @run testng/othervm
  *   -Djdk.internal.foreign.ProgrammableInvoker.USE_SPEC=true
  *   -Djdk.internal.foreign.ProgrammableInvoker.USE_INTRINSICS=true
