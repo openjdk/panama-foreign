c1b9648621bc98edfbc63f5fdac4bb8ecd445415
Sundararajan Athijegannathan <sundararajan.athijegannathan@oracle.com>
2021-04-12 20:51:26 +0530

    code review suggestion

--- src/jdk.incubator.foreign/share/classes/jdk/incubator/foreign/LibraryLookup.java
+++ src/jdk.incubator.foreign/share/classes/jdk/incubator/foreign/LibraryLookup.java
@@ -65,11 +65,6 @@ public interface LibraryLookup {
 
     /**
      * Looks up a symbol with given name in this library. The returned memory address maintains a strong reference to this lookup object.
-     * <p>
-     * This method is <a href="package-summary.html#restricted"><em>restricted</em></a>.
-     * Restricted method are unsafe, and, if used incorrectly, their use might crash
-     * the JVM or, worse, silently result in memory corruption. Thus, clients should refrain from depending on
-     * restricted methods, and use safe and supported functionalities, where possible.
      *
      * @param name the symbol name.
      * @return the memory address associated with the library symbol (if any).
