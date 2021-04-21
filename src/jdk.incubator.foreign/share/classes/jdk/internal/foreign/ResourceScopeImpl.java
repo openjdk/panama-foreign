42de52ad687a960e10805653843bdf71e13a439a
Chris Hegarty <chris.hegarty@oracle.com>
2021-04-21 16:12:38 +0100

    Remove unnecessary abstract override

--- src/jdk.incubator.foreign/share/classes/jdk/internal/foreign/ResourceScopeImpl.java
+++ src/jdk.incubator.foreign/share/classes/jdk/internal/foreign/ResourceScopeImpl.java
@@ -52,9 +52,6 @@ import java.util.Objects;
  */
 public abstract class ResourceScopeImpl implements ResourceScope, ScopedMemoryAccess.Scope, SegmentAllocator {
 
-    @Override
-    public abstract Handle acquire();
-
     final ResourceList resourceList;
 
     @Override
