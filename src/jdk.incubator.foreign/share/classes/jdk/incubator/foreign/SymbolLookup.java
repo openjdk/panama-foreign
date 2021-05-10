1b5f0871fcb430573b85514c80d13a3ce7478b90
Maurizio Cimadamore <maurizio.cimadamore@oracle.com>
2021-05-10 17:51:02 +0100

    Address review comments

--- src/jdk.incubator.foreign/share/classes/jdk/incubator/foreign/SymbolLookup.java
+++ src/jdk.incubator.foreign/share/classes/jdk/incubator/foreign/SymbolLookup.java
@@ -58,35 +58,15 @@ public interface SymbolLookup {
      * restricted methods, and use safe and supported functionalities, where possible.
      *
      * @return a symbol lookup suitable to find symbols in libraries loaded by the caller's classloader.
+     * @throws IllegalCallerException if access to this method occurs from a module {@code M} and the command line option
+     * {@code --enable-native-access} is either absent, or does not mention the module name {@code M}, or
+     * {@code ALL-UNNAMED} in case {@code M} is an unnamed module.
      */
     @CallerSensitive
     static SymbolLookup loaderLookup() {
         Class<?> caller = Reflection.getCallerClass();
         Reflection.ensureNativeAccess(caller);
         ClassLoader loader = Objects.requireNonNull(caller.getClassLoader());
-        return loaderLookup0(loader);
-    }
-
-    /**
-     * Obtains a symbol lookup suitable to find symbols in native libraries associated with the given classloader
-     * (that is, libraries loaded using {@link System#loadLibrary} or {@link System#load}).
-     * <p>
-     * This method is <a href="package-summary.html#restricted"><em>restricted</em></a>.
-     * Restricted method are unsafe, and, if used incorrectly, their use might crash
-     * the JVM or, worse, silently result in memory corruption. Thus, clients should refrain from depending on
-     * restricted methods, and use safe and supported functionalities, where possible.
-     *
-     * @param loader the classloader whose symbol lookup is to be retrieved.
-     * @return a symbol lookup suitable to find symbols in libraries loaded by given classloader.
-     */
-    @CallerSensitive
-    static SymbolLookup loaderLookup(ClassLoader loader) {
-        Reflection.ensureNativeAccess(Reflection.getCallerClass());
-        Objects.requireNonNull(loader);
-        return loaderLookup0(loader);
-    }
-
-    private static SymbolLookup loaderLookup0(ClassLoader loader) {
         return name -> {
             Objects.requireNonNull(name);
             JavaLangAccess javaLangAccess = SharedSecrets.getJavaLangAccess();
