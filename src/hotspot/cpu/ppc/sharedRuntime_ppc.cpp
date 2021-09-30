4d9f6e508093f785f0642a0bd809af166feba7f6
Jorn Vernee <jorn.vernee@oracle.com>
2021-09-30 18:24:07 +0200

    patch up method handle stubs on all platforms

--- src/hotspot/cpu/ppc/sharedRuntime_ppc.cpp
+++ src/hotspot/cpu/ppc/sharedRuntime_ppc.cpp
@@ -1622,8 +1622,11 @@ static void gen_special_dispatch(MacroAssembler* masm,
     member_arg_pos = method->size_of_parameters() - 1;  // trailing MemberName argument
     member_reg = R19_method;  // known to be free at this point
     has_receiver = MethodHandles::ref_kind_has_receiver(ref_kind);
-  } else if (iid == vmIntrinsics::_invokeBasic || iid == vmIntrinsics::_linkToNative) {
+  } else if (iid == vmIntrinsics::_invokeBasic) {
     has_receiver = true;
+  } else if (iid == vmIntrinsics::_linkToNative) {
+    member_arg_pos = method->size_of_parameters() - 1;  // trailing NativeEntryPoint argument
+    member_reg = R19_method;  // known to be free at this point
   } else {
     fatal("unexpected intrinsic id %d", vmIntrinsics::as_int(iid));
   }
