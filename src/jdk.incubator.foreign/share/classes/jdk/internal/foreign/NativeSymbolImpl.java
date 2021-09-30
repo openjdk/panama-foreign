0003838b0f05499144ca8178eb5379ffb7729b6d
Maurizio Cimadamore <maurizio.cimadamore@oracle.com>
2021-09-30 13:38:04 +0100

    Add scope check Add test

--- src/jdk.incubator.foreign/share/classes/jdk/internal/foreign/NativeSymbolImpl.java
+++ src/jdk.incubator.foreign/share/classes/jdk/internal/foreign/NativeSymbolImpl.java
@@ -4,4 +4,10 @@ import jdk.incubator.foreign.MemoryAddress;
 import jdk.incubator.foreign.NativeSymbol;
 import jdk.incubator.foreign.ResourceScope;
 
-public record NativeSymbolImpl(String name, MemoryAddress address, ResourceScope scope) implements NativeSymbol, Scoped { }
+public record NativeSymbolImpl(String name, MemoryAddress address, ResourceScope scope) implements NativeSymbol, Scoped {
+    @Override
+    public MemoryAddress address() {
+        ((ResourceScopeImpl)scope).checkValidState();
+        return address;
+    }
+}
