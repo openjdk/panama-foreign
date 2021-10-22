1e309797d3884c00c26b446b0ded026ab8404a54
Jorn Vernee <jorn.vernee@oracle.com>
2021-10-22 15:46:38 +0200

    Fixup all platform stubs

--- src/hotspot/cpu/x86/universalNativeInvoker_x86_32.cpp
+++ src/hotspot/cpu/x86/universalNativeInvoker_x86_32.cpp
@@ -24,21 +24,13 @@
 #include "precompiled.hpp"
 #include "prims/universalNativeInvoker.hpp"
 
-void ProgrammableInvoker::Generator::generate() {
-  Unimplemented();
-}
-
-address ProgrammableInvoker::generate_adapter(jobject jabi, jobject jlayout) {
-  Unimplemented();
-  return nullptr;
-}
-
 RuntimeStub* ProgrammableInvoker::make_native_invoker(BasicType* signature,
                                                       int num_args,
                                                       BasicType ret_bt,
-                                                      int shadow_space_bytes,
+                                                      const ABIDescriptor& abi,
                                                       const GrowableArray<VMReg>& input_registers,
-                                                      const GrowableArray<VMReg>& output_registers) {
+                                                      const GrowableArray<VMReg>& output_registers,
+                                                      bool is_imr) {
   Unimplemented();
   return nullptr;
 }
--- src/hotspot/cpu/zero/universalNativeInvoker_zero.cpp
+++ src/hotspot/cpu/zero/universalNativeInvoker_zero.cpp
@@ -24,17 +24,13 @@
 #include "precompiled.hpp"
 #include "prims/universalNativeInvoker.hpp"
 
-address ProgrammableInvoker::generate_adapter(jobject jabi, jobject jlayout) {
-  ShouldNotCallThis();
-  return nullptr;
-}
-
 RuntimeStub* ProgrammableInvoker::make_native_invoker(BasicType* signature,
                                                       int num_args,
                                                       BasicType ret_bt,
-                                                      int shadow_space_bytes,
+                                                      const ABIDescriptor& abi,
                                                       const GrowableArray<VMReg>& input_registers,
-                                                      const GrowableArray<VMReg>& output_registers) {
+                                                      const GrowableArray<VMReg>& output_registers,
+                                                      bool is_imr) {
   Unimplemented();
   return nullptr;
 }
