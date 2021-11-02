818dcfc8e7c0383efb3438a19fc7e24bfdbfcae7
Jorn Vernee <jvernee@openjdk.org>
2021-11-02 16:12:30 +0000

    8255903: Enable multi-register return values for native invokers
    Reviewed-by: mcimadamore

--- src/jdk.incubator.foreign/share/classes/jdk/incubator/foreign/FunctionDescriptor.java
+++ src/jdk.incubator.foreign/share/classes/jdk/incubator/foreign/FunctionDescriptor.java
@@ -123,11 +123,26 @@ public sealed class FunctionDescriptor implements Constable permits FunctionDesc
      * @param addedLayouts the argument layouts to append.
      * @return the new function descriptor.
      */
-    public FunctionDescriptor withAppendedArgumentLayouts(MemoryLayout... addedLayouts) {
+    public FunctionDescriptor appendArgumentLayouts(MemoryLayout... addedLayouts) {
+        return insertArgumentLayouts(argLayouts.length, addedLayouts);
+    }
+
+    /**
+     * Create a new function descriptor with the given argument layouts inserted at the given index, into the argument
+     * layout array of this function descriptor.
+     * @param index the index at which to insert the arguments
+     * @param addedLayouts the argument layouts to append.
+     * @return the new function descriptor.
+     * @throws IllegalArgumentException if {@code index < 0 || index > argumentLayouts().size()}.
+     */
+    public FunctionDescriptor insertArgumentLayouts(int index, MemoryLayout... addedLayouts) {
         Objects.requireNonNull(addedLayouts);
         Arrays.stream(addedLayouts).forEach(Objects::requireNonNull);
+        if (index < 0 || index > argLayouts.length)
+            throw new IllegalArgumentException("Index out of bounds: " + index);
         MemoryLayout[] newLayouts = Arrays.copyOf(argLayouts, argLayouts.length + addedLayouts.length);
-        System.arraycopy(addedLayouts, 0, newLayouts, argLayouts.length, addedLayouts.length);
+        System.arraycopy(newLayouts, index, newLayouts, index + addedLayouts.length, argLayouts.length - index);
+        System.arraycopy(addedLayouts, 0, newLayouts, index, addedLayouts.length);
         return new FunctionDescriptor(resLayout, newLayouts);
     }
 
@@ -227,7 +242,12 @@ public sealed class FunctionDescriptor implements Constable permits FunctionDesc
         }
 
         @Override
-        public FunctionDescriptor withAppendedArgumentLayouts(MemoryLayout... addedLayouts) {
+        public FunctionDescriptor appendArgumentLayouts(MemoryLayout... addedLayouts) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public FunctionDescriptor insertArgumentLayouts(int index, MemoryLayout... addedLayouts) {
             throw new UnsupportedOperationException();
         }
 
