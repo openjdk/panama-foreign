a015dc4173c11ddf0937076e34fb5ed7b649aba5
Maurizio Cimadamore <maurizio.cimadamore@oracle.com>
2022-02-03 22:17:54 +0000

    Fix stack popping in assert code

--- src/java.base/share/classes/jdk/internal/foreign/abi/BindingSpecializer.java
+++ src/java.base/share/classes/jdk/internal/foreign/abi/BindingSpecializer.java
@@ -150,7 +150,7 @@ public class BindingSpecializer {
 
         new BindingSpecializer(mv, callerMethodType, callingSequence, abi, leafHandle.type()).specialize();
 
-        mv.visitMaxs(0, 0);
+        mv.visitMaxs(-1, -1);
         mv.visitEnd();
 
         cw.visitEnd();
@@ -198,6 +198,28 @@ public class BindingSpecializer {
         }
         return sb.toString();
     }
+
+    // binding operand stack manipulation
+
+    private void pushType(Class<?> type) {
+        typeStack.push(type);
+    }
+
+    private Class<?> popType() {
+        return typeStack.pop();
+    }
+
+    private Class<?> popType(Class<?> expected) {
+        Class<?> found;
+        if ((found = popType()) != expected) {
+            throw new IllegalStateException(
+                    String.format("Invalid type on binding operand stack; found %s - expected %s",
+                            found.descriptorString(), expected.descriptorString()));
+        }
+        return found;
+    }
+    
+    // specialization
 
     private void specialize() {
         // map of parameter indexes to local var table slots
@@ -259,7 +281,7 @@ public class BindingSpecializer {
                 if (callingSequence.needsReturnBuffer() && i == 0) {
                     assert RETURN_BUFFER_IDX != -1;
                     emitLoad(BasicType.L, RETURN_BUFFER_IDX);
-                    typeStack.push(MemorySegment.class);
+                    pushType(MemorySegment.class);
                 } else {
                     emitGetInput();
                 }
@@ -272,7 +294,7 @@ public class BindingSpecializer {
                 // for upcalls, recipes have a result, which we handle here
                 if (callingSequence.needsReturnBuffer() && i == 0) {
                     // return buffer ptr is wrapped in a MemorySegment above, but not passed to the leaf handle
-                    assert typeStack.pop() == MemorySegment.class;
+                    popType(MemorySegment.class);
                     RETURN_BUFFER_IDX = newLocal(BasicType.L);
                     emitStore(BasicType.L, RETURN_BUFFER_IDX);
                 } else {
@@ -306,7 +328,7 @@ public class BindingSpecializer {
         // return value processing
         if (callingSequence.hasReturnBindings()) {
             if (callingSequence.forUpcall()) {
-                typeStack.push(leafType.returnType());
+                pushType(leafType.returnType());
             }
 
             retBufOffset = 0; // offset for reading from return buffer
@@ -325,7 +347,7 @@ public class BindingSpecializer {
                 assert typeStack.isEmpty();
                 mv.visitInsn(RETURN);
             } else {
-                assert typeStack.pop() == callerMethodType.returnType();
+                popType(callerMethodType.returnType());
                 assert typeStack.isEmpty();
                 emitReturn(callerMethodType.returnType());
             }
@@ -381,7 +403,7 @@ public class BindingSpecializer {
     private void emitGetInput() {
         Class<?> highLevelType = callerMethodType.parameterType(paramIndex);
         emitLoad(BasicType.of(highLevelType), paramIndex2ParamSlot[paramIndex]);
-        typeStack.push(highLevelType);
+        pushType(highLevelType);
         paramIndex++;
     }
 
@@ -393,7 +415,7 @@ public class BindingSpecializer {
     private void emitRestoreReturnValue(Class<?> loadType) {
         assert RET_VAL_IDX != -1;
         emitLoad(BasicType.of(loadType), RET_VAL_IDX);
-        typeStack.push(loadType);
+        pushType(loadType);
     }
 
     private int newLocal(Class<?> type) {
@@ -426,7 +448,7 @@ public class BindingSpecializer {
 
     private void emitToSegment(Binding.ToSegment binding) {
         long size = binding.size();
-        assert typeStack.pop() == MemoryAddress.class;
+        popType(MemoryAddress.class);
 
         emitToRawLongValue();
         emitConst(size);
@@ -434,7 +456,7 @@ public class BindingSpecializer {
         emitCheckCast(ResourceScopeImpl.class);
         emitInvokeStatic(MemoryAddressImpl.class, "ofLongUnchecked", OF_LONG_UNCHECKED_DESC);
 
-        typeStack.push(MemorySegment.class);
+        pushType(MemorySegment.class);
     }
 
     private void emitToRawLongValue() {
@@ -442,9 +464,9 @@ public class BindingSpecializer {
     }
 
     private void emitBoxAddress() {
-        assert typeStack.pop() == long.class;
+        popType(long.class);
         emitInvokeStatic(MemoryAddress.class, "ofLong", OF_LONG_DESC);
-        typeStack.push(MemoryAddress.class);
+        pushType(MemoryAddress.class);
     }
 
     private void emitAllocBuffer(Binding.Allocate binding) {
@@ -455,15 +477,15 @@ public class BindingSpecializer {
             emitLoadInternalAllocator();
         }
         emitAllocateCall(binding.size(), binding.alignment());
-        typeStack.push(MemorySegment.class);
+        pushType(MemorySegment.class);
     }
 
     private void emitBufferStore(Binding.BufferStore bufferStore) {
         Class<?> storeType = bufferStore.type();
         long offset = bufferStore.offset();
 
-        assert typeStack.pop() == storeType;
-        assert typeStack.pop() == MemorySegment.class;
+        popType(storeType);
+        popType(MemorySegment.class);
         BasicType basicStoreType = BasicType.of(storeType);
         int valueIdx = newLocal(basicStoreType);
         emitStore(basicStoreType, valueIdx);
@@ -479,7 +501,7 @@ public class BindingSpecializer {
     // VM_STORE and VM_LOAD are emulated, which is different for down/upcalls
     private void emitVMStore(Binding.VMStore vmStore) {
         Class<?> storeType = vmStore.type();
-        assert typeStack.pop() == storeType;
+        popType(storeType);
 
         if (callingSequence.forDowncall()) {
             // processing arg
@@ -520,7 +542,7 @@ public class BindingSpecializer {
                 String descriptor = methodType(loadType, valueLayoutType, long.class).descriptorString();
                 emitInvokeInterface(MemorySegment.class, "get", descriptor);
                 retBufOffset += abi.arch.typeSize(vmLoad.storage().type());
-                typeStack.push(loadType);
+                pushType(loadType);
             }
         } else {
             // processing arg
@@ -530,34 +552,35 @@ public class BindingSpecializer {
     private void emitDupBinding() {
         Class<?> dupType = typeStack.peek();
         emitDup(BasicType.of(dupType));
-        typeStack.push(dupType);
+        pushType(dupType);
     }
 
     private void emitUnboxAddress() {
-        assert Addressable.class.isAssignableFrom(typeStack.pop());
+        Class<?> stackType = popType();
+        assert Addressable.class.isAssignableFrom(stackType);
         emitInvokeInterface(Addressable.class, "address", ADDRESS_DESC);
         emitToRawLongValue();
-        typeStack.push(long.class);
+        pushType(long.class);
     }
 
     private void emitBufferLoad(Binding.BufferLoad bufferLoad) {
         Class<?> loadType = bufferLoad.type();
         long offset = bufferLoad.offset();
 
-        assert typeStack.pop() == MemorySegment.class;
+        popType(MemorySegment.class);
 
         Class<?> valueLayoutType = emitLoadLayoutConstant(loadType);
         emitConst(offset);
         String descriptor = methodType(loadType, valueLayoutType, long.class).descriptorString();
         emitInvokeInterface(MemorySegment.class, "get", descriptor);
-        typeStack.push(loadType);
+        pushType(loadType);
     }
 
     private void emitCopyBuffer(Binding.Copy copy) {
         long size = copy.size();
         long alignment = copy.alignment();
 
-        assert typeStack.pop() == MemorySegment.class;
+        popType(MemorySegment.class);
 
         // operand/srcSegment is on the stack
         // generating a call to:
@@ -575,7 +598,7 @@ public class BindingSpecializer {
         emitInvokeStatic(MemorySegment.class, "copy", COPY_DESC);
 
         emitLoad(BasicType.L, storeIdx);
-        typeStack.push(MemorySegment.class);
+        pushType(MemorySegment.class);
     }
 
     private void emitAllocateCall(long size, long alignment) {
