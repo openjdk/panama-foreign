4f18f441ed783bb720d2d8e93c26b9878f0e7b33
Jorn Vernee <jorn.vernee@oracle.com>
2022-08-04 18:09:52 +0200

    Tweaks

--- src/hotspot/share/code/codeBlob.cpp
+++ src/hotspot/share/code/codeBlob.cpp
@@ -790,9 +790,9 @@ void UpcallStub::verify() {
 void UpcallStub::print_on(outputStream* st) const {
   RuntimeBlob::print_on(st);
   print_value_on(st);
+  Disassembler::decode((RuntimeBlob*)this, st);
 }
 
 void UpcallStub::print_value_on(outputStream* st) const {
-  st->print_cr("UpcallStub (" INTPTR_FORMAT  "): %s", p2i(this), name());
-  Disassembler::decode((RuntimeBlob*)this, st);
+  st->print_cr("UpcallStub (" INTPTR_FORMAT  ") used for %s", p2i(this), name());
 }
