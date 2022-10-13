9189baa85912fbdf1a9cea0b1badf8ad12a8732c
Jorn Vernee <jorn.vernee@oracle.com>
2022-10-13 14:27:20 +0200

    stubs on other platforms

--- src/hotspot/cpu/s390/vmstorage_s390.inline.hpp
+++ src/hotspot/cpu/s390/vmstorage_s390.inline.hpp
@@ -30,12 +30,20 @@
 #include "prims/vmstorageBase.inline.hpp"
 
 enum class StorageType : int8_t {
-  STACK = 0
+  STACK = 0,
+  PLACEHOLDER = 1,
+// special locations used only by native code
+  FRAME_DATA = PLACEHOLDER + 1,
+  INVALID = -1
 };
 
-constexpr inline StorageType VMStorage::stack_type() {
-  return StorageType::STACK;
+// need to define this before constructing VMStorage (below)
+constexpr inline bool VMStorage::is_reg(StorageType type) {
+   return false;
 }
+constexpr inline StorageType VMStorage::stack_type() { return StorageType::STACK; }
+constexpr inline StorageType VMStorage::placeholder_type() { return StorageType::PLACEHOLDER; }
+constexpr inline StorageType VMStorage::frame_data_type() { return StorageType::FRAME_DATA; }
 
 inline VMStorage as_VMStorage(VMReg reg) {
   ShouldNotReachHere();
