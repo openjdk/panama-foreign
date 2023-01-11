08225e4f5e00837e974098a933f27267f3c0dbc5
Saint Wesonga <sawesong@microsoft.com>
2023-01-11 04:23:33 +0000

    8295290: Add Windows ARM64 ABI support to the Foreign Function & Memory API
    Reviewed-by: jvernee

--- src/java.base/share/classes/jdk/internal/foreign/CABI.java
+++ src/java.base/share/classes/jdk/internal/foreign/CABI.java
@@ -32,7 +32,8 @@ public enum CABI {
     SYS_V,
     WIN_64,
     LINUX_AARCH_64,
-    MAC_OS_AARCH_64;
+    MAC_OS_AARCH_64,
+    WIN_AARCH_64;
 
     private static final CABI ABI;
     private static final String ARCH;
@@ -54,6 +55,8 @@ public enum CABI {
         } else if (ARCH.equals("aarch64")) {
             if (OS.startsWith("Mac")) {
                 ABI = MAC_OS_AARCH_64;
+            } else if (OS.startsWith("Windows")) {
+                ABI = WIN_AARCH_64;
             } else {
                 // The Linux ABI follows the standard AAPCS ABI
                 ABI = LINUX_AARCH_64;
--- src/java.base/share/classes/jdk/internal/foreign/abi/SharedUtils.java
+++ src/java.base/share/classes/jdk/internal/foreign/abi/SharedUtils.java
@@ -30,6 +30,7 @@ import jdk.internal.access.SharedSecrets;
 import jdk.internal.foreign.CABI;
 import jdk.internal.foreign.abi.aarch64.linux.LinuxAArch64Linker;
 import jdk.internal.foreign.abi.aarch64.macos.MacOsAArch64Linker;
+import jdk.internal.foreign.abi.aarch64.windows.WindowsAArch64Linker;
 import jdk.internal.foreign.abi.x64.sysv.SysVx64Linker;
 import jdk.internal.foreign.abi.x64.windows.Windowsx64Linker;
 import jdk.internal.vm.annotation.ForceInline;
@@ -182,6 +183,7 @@ public final class SharedUtils {
             case SYS_V -> SysVx64Linker.getInstance();
             case LINUX_AARCH_64 -> LinuxAArch64Linker.getInstance();
             case MAC_OS_AARCH_64 -> MacOsAArch64Linker.getInstance();
+            case WIN_AARCH_64 -> WindowsAArch64Linker.getInstance();
         };
     }
 
