e51a2eca250f04d55039371220bb495f1defe3ec
Per Minborg <pminborg@openjdk.org>
2023-02-13 10:20:53 +0000

    ValueLayouts: Improve test coverage and more
    Reviewed-by: jvernee

--- src/java.base/share/classes/jdk/internal/foreign/layout/ValueLayouts.java
+++ src/java.base/share/classes/jdk/internal/foreign/layout/ValueLayouts.java
@@ -1,5 +1,5 @@
 /*
- *  Copyright (c) 2019, 2022, Oracle and/or its affiliates. All rights reserved.
+ *  Copyright (c) 2019, 2023, Oracle and/or its affiliates. All rights reserved.
  *  DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  *  This code is free software; you can redistribute it and/or modify it
@@ -78,7 +78,7 @@ public final class ValueLayouts {
             super(bitSize, bitAlignment, name);
             this.carrier = carrier;
             this.order = order;
-            checkCarrierSize(carrier, bitSize);
+            assertCarrierSize(carrier, bitSize);
         }
 
         /**
@@ -108,13 +108,9 @@ public final class ValueLayouts {
 
         @Override
         public boolean equals(Object other) {
-            if (this == other) {
-                return true;
-            }
-            if (!super.equals(other)) {
-                return false;
-            }
-            return other instanceof AbstractValueLayout<?> otherValue &&
+            return this == other ||
+                    other instanceof AbstractValueLayout<?> otherValue &&
+                    super.equals(other) &&
                     carrier.equals(otherValue.carrier) &&
                     order.equals(otherValue.order);
         }
@@ -149,22 +145,18 @@ public final class ValueLayouts {
         @Override
         abstract V dup(long bitAlignment, Optional<String> name);
 
-        static void checkCarrierSize(Class<?> carrier, long size) {
-            if (!isValidCarrier(carrier)) {
-                throw new IllegalArgumentException("Invalid carrier: " + carrier.getName());
-            }
-            if (carrier == MemorySegment.class && size != ADDRESS_SIZE_BITS) {
-                throw new IllegalArgumentException("Address size mismatch: " + ADDRESS_SIZE_BITS + " != " + size);
-            }
-            if (carrier.isPrimitive()) {
-                int expectedSize = carrier == boolean.class ? 8 : Wrapper.forPrimitiveType(carrier).bitWidth();
-                if (size != expectedSize) {
-                    throw new IllegalArgumentException("Carrier size mismatch: " + carrier.getName() + " != " + size);
-                }
-            }
+        static void assertCarrierSize(Class<?> carrier, long bitSize) {
+            assert isValidCarrier(carrier);
+            assert carrier != MemorySegment.class
+                    // MemorySegment bitSize must always equal ADDRESS_SIZE_BITS
+                    || bitSize == ADDRESS_SIZE_BITS;
+            assert !carrier.isPrimitive() ||
+                    // Primitive class bitSize must always correspond
+                    bitSize == (carrier == boolean.class ? 8 : Wrapper.forPrimitiveType(carrier).bitWidth());
         }
 
         static boolean isValidCarrier(Class<?> carrier) {
+            // void.class is not valid
             return carrier == boolean.class
                     || carrier == byte.class
                     || carrier == short.class
@@ -175,7 +167,6 @@ public final class ValueLayouts {
                     || carrier == double.class
                     || carrier == MemorySegment.class;
         }
-
 
         @ForceInline
         public final VarHandle accessHandle() {
--- test/jdk/java/foreign/TestLayouts.java
+++ test/jdk/java/foreign/TestLayouts.java
@@ -32,16 +32,12 @@ import java.lang.foreign.*;
 import java.lang.invoke.VarHandle;
 import java.nio.ByteOrder;
 import java.util.List;
-import java.util.function.Function;
 import java.util.function.LongFunction;
 import java.util.stream.Stream;
 
 import org.testng.annotations.*;
 
-import static java.lang.foreign.ValueLayout.JAVA_BYTE;
-import static java.lang.foreign.ValueLayout.JAVA_INT;
-import static java.lang.foreign.ValueLayout.JAVA_LONG;
-import static java.lang.foreign.ValueLayout.JAVA_SHORT;
+import static java.lang.foreign.ValueLayout.*;
 import static org.testng.Assert.*;
 
 public class TestLayouts {
@@ -49,6 +45,33 @@ public class TestLayouts {
     @Test(dataProvider = "badAlignments", expectedExceptions = IllegalArgumentException.class)
     public void testBadLayoutAlignment(MemoryLayout layout, long alignment) {
         layout.withBitAlignment(alignment);
+    }
+
+    @Test(dataProvider = "basicLayoutsAndAddress")
+    public void testNotEquals(MemoryLayout layout) {
+
+        // Use another Type
+        MemoryLayout differentType = MemoryLayout.paddingLayout(8);
+        assertFalse(layout.equals(differentType));
+
+        // Use another name
+        MemoryLayout differentName = layout.withName("CustomName");
+        assertFalse(layout.equals(differentName));
+
+        // Swap endian
+        MemoryLayout differentOrder = JAVA_INT.withOrder(JAVA_INT.order() == ByteOrder.BIG_ENDIAN ? ByteOrder.LITTLE_ENDIAN : ByteOrder.BIG_ENDIAN);
+        assertFalse(layout.equals(differentOrder));
+
+        // Something totally different
+        assertFalse(layout.equals("A"));
+
+        // Null
+        assertFalse(layout.equals(null));
+    }
+
+    public void testTargetLayoutEquals() {
+        MemoryLayout differentTargetLayout = ADDRESS.withTargetLayout(JAVA_CHAR);
+        assertFalse(ADDRESS.equals(differentTargetLayout));
     }
 
     @Test
@@ -276,6 +299,13 @@ public class TestLayouts {
     @DataProvider(name = "basicLayouts")
     public Object[][] basicLayouts() {
         return Stream.of(basicLayouts)
+                .map(l -> new Object[] { l })
+                .toArray(Object[][]::new);
+    }
+
+    @DataProvider(name = "basicLayoutsAndAddress")
+    public Object[][] basicLayoutsAndAddress() {
+        return Stream.concat(Stream.of(basicLayouts), Stream.of(ADDRESS))
                 .map(l -> new Object[] { l })
                 .toArray(Object[][]::new);
     }
