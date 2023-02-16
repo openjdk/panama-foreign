2c378000405d65345bb799bccdc1facc06b63f8d
Per Minborg <pminborg@openjdk.org>
2023-02-16 14:44:37 +0000

    Simplify code now that bitSize is always aligned to byte boundaries
    Reviewed-by: mcimadamore

--- src/java.base/share/classes/jdk/internal/foreign/abi/x64/windows/CallArranger.java
+++ src/java.base/share/classes/jdk/internal/foreign/abi/x64/windows/CallArranger.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020, 2022, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2020, 2023, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -41,6 +41,7 @@ import java.lang.foreign.FunctionDescriptor;
 import java.lang.foreign.GroupLayout;
 import java.lang.foreign.MemoryLayout;
 import java.lang.foreign.MemorySegment;
+import java.lang.foreign.ValueLayout;
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodType;
 import java.util.List;
@@ -276,9 +277,10 @@ public class CallArranger {
                     break;
                 }
                 case POINTER: {
+                    ValueLayout.OfAddress addressLayout = (ValueLayout.OfAddress)layout;
                     VMStorage storage = storageCalculator.nextStorage(StorageType.INTEGER);
                     bindings.vmLoad(storage, long.class)
-                            .boxAddressRaw(Utils.pointeeSize(layout), Utils.pointeeAlign(layout));
+                            .boxAddressRaw(Utils.pointeeByteSize(addressLayout), Utils.pointeeByteAlign(addressLayout));
                     break;
                 }
                 case INTEGER: {
