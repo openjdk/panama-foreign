38f4bfe7435402f4242a1c1a9286d1399ecbfdb5
Per Minborg <pminborg@openjdk.org>
2023-05-29 18:20:12 +0000

    Use switch instead of if-statements in MemoryInspection classes
    Reviewed-by: mcimadamore

--- src/java.base/share/classes/jdk/internal/foreign/MemoryInspectionUtil.java
+++ src/java.base/share/classes/jdk/internal/foreign/MemoryInspectionUtil.java
@@ -70,95 +70,71 @@ final class MemoryInspectionUtil {
                                   ViewState state,
                                   String suffix) {
 
-        // TODO: Replace with "patterns in switch statement" once this becomes available.
-
-        if (layout instanceof ValueLayout.OfBoolean ofBoolean) {
-            action.accept(renderValueLayout(state, ofBoolean, renderer.apply(ofBoolean, segment.get(ofBoolean, state.indexAndAdd(ofBoolean))), suffix));
-            return;
-        }
-        if (layout instanceof ValueLayout.OfByte ofByte) {
-            action.accept(renderValueLayout(state, ofByte, renderer.apply(ofByte, segment.get(ofByte, state.indexAndAdd(ofByte))), suffix));
-            return;
-        }
-        if (layout instanceof ValueLayout.OfShort ofShort) {
-            action.accept(renderValueLayout(state, ofShort, renderer.apply(ofShort, segment.get(ofShort, state.indexAndAdd(ofShort))), suffix));
-            return;
-        }
-        if (layout instanceof ValueLayout.OfInt ofInt) {
-            action.accept(renderValueLayout(state, ofInt, renderer.apply(ofInt, segment.get(ofInt, state.indexAndAdd(ofInt))), suffix));
-            return;
-        }
-        if (layout instanceof ValueLayout.OfLong ofLong) {
-            action.accept(renderValueLayout(state, ofLong, renderer.apply(ofLong, segment.get(ofLong, state.indexAndAdd(ofLong))), suffix));
-            return;
-        }
-        if (layout instanceof ValueLayout.OfFloat ofFloat) {
-            action.accept(renderValueLayout(state, ofFloat, renderer.apply(ofFloat, segment.get(ofFloat, state.indexAndAdd(ofFloat))), suffix));
-            return;
-        }
-        if (layout instanceof ValueLayout.OfDouble ofDouble) {
-            action.accept(renderValueLayout(state, ofDouble, renderer.apply(ofDouble, segment.get(ofDouble, state.indexAndAdd(ofDouble))), suffix));
-            return;
-        }
-        if (layout instanceof ValueLayout.OfChar ofChar) {
-            action.accept(renderValueLayout(state, ofChar, renderer.apply(ofChar, segment.get(ofChar, state.indexAndAdd(ofChar))), suffix));
-            return;
-        }
-        if (layout instanceof AddressLayout ofAddress) {
-            action.accept(renderValueLayout(state, ofAddress, renderer.apply(ofAddress, segment.get(ofAddress, state.indexAndAdd(ofAddress))), suffix));
-            return;
-        }
-        if (layout instanceof PaddingLayout paddingLayout) {
-            action.accept(state.indentSpaces() + paddingLayout.byteSize() + " padding bytes");
-            state.indexAndAdd(paddingLayout);
-            return;
-        }
-        if (layout instanceof GroupLayout groupLayout) {
-
-            /* Strictly, we should provide all permutations of unions.
-             * So, if we have a union U =  (A|B),(C|D) then we should present:
-             * (A,C), (A,D), (B,C) and (B,D)
-             */
-
-            final var separator = groupLayout instanceof StructLayout
-                    ? ","  // Struct separator
-                    : "|"; // Union separator
-
-            action.accept(indentedLabel(state, groupLayout) + " {");
-            state.incrementIndent();
-            final var members = groupLayout.memberLayouts();
-            final long initialIndex = state.index();
-            long maxIndex = initialIndex;
-            for (int i = 0; i < members.size(); i++) {
-                if (groupLayout instanceof UnionLayout) {
-                    // If it is a union, we need to reset the index for each member
-                    state.index(initialIndex);
-                    // We record the max index used for any union member so we can leave off from there
-                    maxIndex = Math.max(maxIndex, state.index());
-                }
-                toString0(segment, members.get(i), renderer, action, state, (i != (members.size() - 1)) ? separator : "");
-                if (groupLayout instanceof UnionLayout) {
-                    // This is the best we can do.
-                    state.index(maxIndex);
-                }
+        switch (layout) {
+            case ValueLayout.OfBoolean ofBoolean ->
+                    action.accept(renderValueLayout(state, ofBoolean, renderer.apply(ofBoolean, segment.get(ofBoolean, state.indexAndAdd(ofBoolean))), suffix));
+            case ValueLayout.OfByte ofByte ->
+                    action.accept(renderValueLayout(state, ofByte, renderer.apply(ofByte, segment.get(ofByte, state.indexAndAdd(ofByte))), suffix));
+            case ValueLayout.OfShort ofShort ->
+                    action.accept(renderValueLayout(state, ofShort, renderer.apply(ofShort, segment.get(ofShort, state.indexAndAdd(ofShort))), suffix));
+            case ValueLayout.OfChar ofChar ->
+                    action.accept(renderValueLayout(state, ofChar, renderer.apply(ofChar, segment.get(ofChar, state.indexAndAdd(ofChar))), suffix));
+            case ValueLayout.OfInt ofInt ->
+                    action.accept(renderValueLayout(state, ofInt, renderer.apply(ofInt, segment.get(ofInt, state.indexAndAdd(ofInt))), suffix));
+            case ValueLayout.OfLong ofLong ->
+                    action.accept(renderValueLayout(state, ofLong, renderer.apply(ofLong, segment.get(ofLong, state.indexAndAdd(ofLong))), suffix));
+            case ValueLayout.OfFloat ofFloat ->
+                    action.accept(renderValueLayout(state, ofFloat, renderer.apply(ofFloat, segment.get(ofFloat, state.indexAndAdd(ofFloat))), suffix));
+            case ValueLayout.OfDouble ofDouble ->
+                    action.accept(renderValueLayout(state, ofDouble, renderer.apply(ofDouble, segment.get(ofDouble, state.indexAndAdd(ofDouble))), suffix));
+            case AddressLayout addressLayout ->
+                    action.accept(renderValueLayout(state, addressLayout, renderer.apply(addressLayout, segment.get(addressLayout, state.indexAndAdd(addressLayout))), suffix));
+            case PaddingLayout paddingLayout -> {
+                action.accept(state.indentSpaces() + paddingLayout.byteSize() + " padding bytes");
+                state.indexAndAdd(paddingLayout);
             }
-            state.decrementIndent();
-            action.accept(state.indentSpaces() + "}" + suffix);
-            return;
-        }
-        if (layout instanceof SequenceLayout sequenceLayout) {
-            action.accept(indentedLabel(state, sequenceLayout) + " [");
-            state.incrementIndent();
-            final long elementCount = sequenceLayout.elementCount();
-            for (long i = 0; i < elementCount; i++) {
-                toString0(segment, sequenceLayout.elementLayout(), renderer, action, state, (i != (elementCount - 1L)) ? "," : "");
+            case GroupLayout groupLayout -> {
+                /* Strictly, we should provide all permutations of unions.
+                 * So, if we have a union U =  (A|B),(C|D) then we should present:
+                 * (A,C), (A,D), (B,C) and (B,D)
+                 */
+
+                final var separator = groupLayout instanceof StructLayout
+                        ? ","  // Struct separator
+                        : "|"; // Union separator
+
+                action.accept(indentedLabel(state, groupLayout) + " {");
+                state.incrementIndent();
+                final var members = groupLayout.memberLayouts();
+                final long initialIndex = state.index();
+                long maxIndex = initialIndex;
+                for (int i = 0; i < members.size(); i++) {
+                    if (groupLayout instanceof UnionLayout) {
+                        // If it is a union, we need to reset the index for each member
+                        state.index(initialIndex);
+                        // We record the max index used for any union member so we can leave off from there
+                        maxIndex = Math.max(maxIndex, state.index());
+                    }
+                    toString0(segment, members.get(i), renderer, action, state, (i != (members.size() - 1)) ? separator : "");
+                    if (groupLayout instanceof UnionLayout) {
+                        // This is the best we can do.
+                        state.index(maxIndex);
+                    }
+                }
+                state.decrementIndent();
+                action.accept(state.indentSpaces() + "}" + suffix);
             }
-            state.decrementIndent();
-            action.accept(state.indentSpaces() + "]" + suffix);
-            return;
+            case SequenceLayout sequenceLayout -> {
+                action.accept(indentedLabel(state, sequenceLayout) + " [");
+                state.incrementIndent();
+                final long elementCount = sequenceLayout.elementCount();
+                for (long i = 0; i < elementCount; i++) {
+                    toString0(segment, sequenceLayout.elementLayout(), renderer, action, state, (i != (elementCount - 1L)) ? "," : "");
+                }
+                state.decrementIndent();
+                action.accept(state.indentSpaces() + "]" + suffix);
+            }
         }
-        action.accept(state.indentSpaces() + "Unknown layout: " + layout + " at index " + state.index());
-        state.indexAndAdd(layout);
     }
 
     static String renderValueLayout(ViewState state,
@@ -227,36 +203,20 @@ final class MemoryInspectionUtil {
             requireNonNull(layout);
             requireNonNull(o);
 
-            // TODO: Replace with "patterns in switch statement" once this becomes available.
-
-            if (layout instanceof ValueLayout.OfBoolean ofBoolean && o instanceof Boolean b) {
-                return Boolean.toString(b);
-            }
-            if (layout instanceof ValueLayout.OfByte ofByte && o instanceof Byte b) {
-                return Byte.toString(b);
-            }
-            if (layout instanceof ValueLayout.OfShort ofShort && o instanceof Short s) {
-                return Short.toString(s);
-            }
-            if (layout instanceof ValueLayout.OfInt ofInt && o instanceof Integer i) {
-                return Integer.toString(i);
-            }
-            if (layout instanceof ValueLayout.OfLong ofLong && o instanceof Long l) {
-                return Long.toString(l);
-            }
-            if (layout instanceof ValueLayout.OfFloat ofFloat && o instanceof Float f) {
-                return Float.toString(f);
-            }
-            if (layout instanceof ValueLayout.OfDouble ofDouble && o instanceof Double d) {
-                return Double.toString(d);
-            }
-            if (layout instanceof ValueLayout.OfChar ofChar && o instanceof Character c) {
-                return Character.toString(c);
-            }
-            if (layout instanceof AddressLayout ofAddress && o instanceof MemorySegment m) {
-                return String.format("0x%0" + (ValueLayout.ADDRESS.byteSize() * 2) + "X", m.address());
-            }
-            throw new UnsupportedOperationException("layout " + layout + " for " + o.getClass().getName() + " not supported");
+            return switch (layout) {
+                case ValueLayout.OfBoolean __ when o instanceof Boolean b -> Boolean.toString(b);
+                case ValueLayout.OfByte __ when o instanceof Byte b -> Byte.toString(b);
+                case ValueLayout.OfShort __ when o instanceof Short s -> Short.toString(s);
+                case ValueLayout.OfChar __ when o instanceof Character c -> Character.toString(c);
+                case ValueLayout.OfInt __ when o instanceof Integer i -> Integer.toString(i);
+                case ValueLayout.OfLong __ when o instanceof Long l -> Long.toString(l);
+                case ValueLayout.OfFloat __ when o instanceof Float f -> Float.toString(f);
+                case ValueLayout.OfDouble __ when o instanceof Double d -> Double.toString(d);
+                case AddressLayout __ when o instanceof MemorySegment m ->
+                        String.format("0x%0" + (ValueLayout.ADDRESS.byteSize() * 2) + "X", m.address());
+                default ->
+                        throw new UnsupportedOperationException("layout " + layout + " for " + o.getClass().getName() + " not supported");
+            };
         }
 
         @Override
