585a1795677e31b3871bafb2d2d6f83a4b615d5e
Per Minborg <pminborg@openjdk.org>
2023-08-07 10:59:49 +0000

    8313238: Improve the performance of strlen for null-terminated strings
    Reviewed-by: jvernee

--- src/java.base/share/classes/jdk/internal/foreign/StringSupport.java
+++ src/java.base/share/classes/jdk/internal/foreign/StringSupport.java
@@ -25,18 +25,43 @@
 
 package jdk.internal.foreign;
 
+import jdk.internal.util.ArraysSupport;
+
+import java.lang.foreign.FunctionDescriptor;
+import java.lang.foreign.Linker;
 import java.lang.foreign.MemorySegment;
+import java.lang.invoke.MethodHandle;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
+import java.util.Objects;
 
+import static java.lang.foreign.ValueLayout.ADDRESS;
 import static java.lang.foreign.ValueLayout.JAVA_BYTE;
-import static java.lang.foreign.ValueLayout.JAVA_SHORT;
 import static java.lang.foreign.ValueLayout.JAVA_INT;
+import static java.lang.foreign.ValueLayout.JAVA_SHORT;
 
 /**
  * Miscellaneous functions to read and write strings, in various charsets.
  */
 public class StringSupport {
+
+    // Maximum segment byte size for which a trivial method will be invoked.
+    private static final long MAX_TRIVIAL_SIZE = 1024L;
+    private static final MethodHandle STRNLEN_TRIVIAL;
+    private static final MethodHandle STRNLEN;
+    private static final boolean SIZE_T_IS_INT;
+
+    static {
+        var size_t = Objects.requireNonNull(Linker.nativeLinker().canonicalLayouts().get("size_t"));
+        Linker linker = Linker.nativeLinker();
+        var strnlen = linker.defaultLookup().find("strnlen").orElseThrow();
+        var description = FunctionDescriptor.of(size_t, ADDRESS, size_t);
+
+        STRNLEN_TRIVIAL = linker.downcallHandle(strnlen, description, Linker.Option.isTrivial());
+        STRNLEN = linker.downcallHandle(strnlen, description);
+        SIZE_T_IS_INT = (size_t.byteSize() == Integer.BYTES);
+    }
+
     public static String read(MemorySegment segment, long offset, Charset charset) {
         return switch (CharsetKind.of(charset)) {
             case SINGLE_BYTE -> readFast_byte(segment, offset, charset);
@@ -55,7 +80,7 @@ public class StringSupport {
         }
     }
     private static String readFast_byte(MemorySegment segment, long offset, Charset charset) {
-        long len = strlen_byte(segment, offset);
+        long len = native_strlen_byte(segment, offset);
         byte[] bytes = new byte[(int)len];
         MemorySegment.copy(segment, JAVA_BYTE, offset, bytes, 0, (int)len);
         return new String(bytes, charset);
@@ -93,15 +118,83 @@ public class StringSupport {
         segment.set(JAVA_INT, offset + bytes.length, 0);
     }
 
-    private static int strlen_byte(MemorySegment segment, long start) {
+    private static int native_strlen_byte(MemorySegment segment, long start) {
+        if (start > 0) {
+            segment = segment.asSlice(start);
+        }
+        long segmentSize = segment.byteSize();
+        final long len;
+        if (SIZE_T_IS_INT) {
+            if (segmentSize < MAX_TRIVIAL_SIZE) {
+                len = strnlen_int_trivial(segment, segmentSize);
+            } else if (segmentSize < Integer.MAX_VALUE * 2L) { // size_t is unsigned
+                len = strnlen_int(segment, segmentSize);
+            } else {
+                // There is no way to express the max size in the native method using an int so, revert
+                // to a Java method. It is possible to use a reduction of several STRNLEN invocations
+                // in a future optimization.
+                len = strlen_byte(segment);
+            }
+        } else {
+            len = segmentSize < MAX_TRIVIAL_SIZE
+                    ? strnlen_long_trivial(segment, segmentSize)
+                    : strnlen_long(segment, segmentSize);
+        }
+        if (len > ArraysSupport.SOFT_MAX_ARRAY_LENGTH) {
+            throw newIaeStringTooLarge();
+        }
+        return (int)len;
+    }
+
+    static long strnlen_int_trivial(MemorySegment segment, long size) {
+        try {
+            return Integer.toUnsignedLong((int)STRNLEN_TRIVIAL.invokeExact(segment, (int)size));
+        } catch (RuntimeException | Error e) {
+            throw e;
+        } catch (Throwable e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+
+    static long strnlen_int(MemorySegment segment, long size) {
+        try {
+            return Integer.toUnsignedLong((int)STRNLEN.invokeExact(segment, (int)size));
+        } catch (RuntimeException | Error e) {
+            throw e;
+        } catch (Throwable e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+
+    static long strnlen_long_trivial(MemorySegment segment, long size) {
+        try {
+            return (long)STRNLEN_TRIVIAL.invokeExact(segment, size);
+        } catch (RuntimeException | Error e) {
+            throw e;
+        } catch (Throwable e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+
+    static long strnlen_long(MemorySegment segment, long size) {
+        try {
+            return (long)STRNLEN.invokeExact(segment, size);
+        } catch (RuntimeException | Error e) {
+            throw e;
+        } catch (Throwable e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+
+    private static int strlen_byte(MemorySegment segment) {
         // iterate until overflow (String can only hold a byte[], whose length can be expressed as an int)
-        for (int offset = 0; offset >= 0; offset++) {
-            byte curr = segment.get(JAVA_BYTE, start + offset);
+        for (int offset = 0; offset >= 0; offset += 2) {
+            short curr = segment.get(JAVA_SHORT, offset);
             if (curr == 0) {
                 return offset;
             }
         }
-        throw new IllegalArgumentException("String too large");
+        throw newIaeStringTooLarge();
     }
 
     private static int strlen_short(MemorySegment segment, long start) {
@@ -112,7 +205,7 @@ public class StringSupport {
                 return offset;
             }
         }
-        throw new IllegalArgumentException("String too large");
+        throw newIaeStringTooLarge();
     }
 
     private static int strlen_int(MemorySegment segment, long start) {
@@ -123,7 +216,7 @@ public class StringSupport {
                 return offset;
             }
         }
-        throw new IllegalArgumentException("String too large");
+        throw newIaeStringTooLarge();
     }
 
     public enum CharsetKind {
@@ -153,4 +246,9 @@ public class StringSupport {
             }
         }
     }
+
+    private static IllegalArgumentException newIaeStringTooLarge() {
+        return new IllegalArgumentException("String too large");
+    }
+
 }
