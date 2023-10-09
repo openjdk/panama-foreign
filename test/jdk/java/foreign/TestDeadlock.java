cccdebf41b0d631d3ab4a76f0262aea28651432e
Jorn Vernee <jorn.vernee@oracle.com>
2023-10-09 21:08:46 +0200

    Add test for FileChannel::map

--- test/jdk/java/foreign/TestDeadlock.java
+++ test/jdk/java/foreign/TestDeadlock.java
@@ -22,32 +22,63 @@
  */
 
 /*
- * @test
- * @run main/othervm/timeout=5 --enable-native-access=ALL-UNNAMED -Xlog:class+init TestDeadlock
+ * @test id=Arena_allocateFrom
+ * @run main/othervm/timeout=5 --enable-native-access=ALL-UNNAMED -Xlog:class+init TestDeadlock Arena
+ */
+
+/*
+ * @test id=FileChannel_map
+ * @run main/othervm/timeout=5 --enable-native-access=ALL-UNNAMED -Xlog:class+init TestDeadlock FileChannel
  */
 
 import java.lang.foreign.*;
+import java.io.IOException;
+import java.nio.channels.FileChannel;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 import java.util.concurrent.CountDownLatch;
 
 public class TestDeadlock {
     public static void main(String[] args) throws Throwable {
         CountDownLatch latch = new CountDownLatch(2);
 
-        Thread t1 = Thread.ofPlatform().start(() -> {
-            Arena arena = Arena.global();
-            arena.scope(); // init ArenaImpl
-            ValueLayout.JAVA_INT.byteSize(); // init ValueLayout (and impls)
-            latch.countDown();
-            try {
-                latch.await();
-            } catch(InterruptedException e) {
-                throw new RuntimeException(e);
-            }
+        Runnable tester = switch (args[0]) {
+            case "Arena" -> () -> {
+                Arena arena = Arena.global();
+                arena.scope(); // init ArenaImpl
+                ValueLayout.JAVA_INT.byteSize(); // init ValueLayout (and impls)
+                latch.countDown();
+                try {
+                    latch.await();
+                } catch(InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
 
-            // Access ArenaImpl -> NativeMemorySegmentImpl -> MemorySegment
-            arena.allocateFrom(ValueLayout.JAVA_INT, 42);
-        });
+                // Access ArenaImpl -> NativeMemorySegmentImpl -> MemorySegment
+                arena.allocateFrom(ValueLayout.JAVA_INT, 42);
+            };
+            case "FileChannel" -> () -> {
+                try {
+                    Arena arena = Arena.global();
+                    Path p = Files.createFile(Path.of("test.out"));
 
+                    try (FileChannel channel = FileChannel.open(p, StandardOpenOption.READ, StandardOpenOption.WRITE)) {
+                        channel.map(FileChannel.MapMode.READ_WRITE, 0, 4); // create MappedByteBuffer to initialize other things
+                        latch.countDown();
+                        latch.await();
+
+                        // Access MappedMemorySegmentImpl -> MemorySegment
+                        channel.map(FileChannel.MapMode.READ_WRITE, 0, 4, arena);
+                    }
+                } catch(InterruptedException | IOException e) {
+                    throw new RuntimeException(e);
+                }
+            };
+            default -> throw new IllegalArgumentException("Unknown test selection: " + args[0]);
+        };
+
+        Thread t1 = Thread.ofPlatform().start(tester);
         Thread t2 = Thread.ofPlatform().start(() -> {
             latch.countDown();
             try {
